d67d29aefe9bae58947b4b12c587b81735b0bfec
TemurEShtemirov <temureshtemirov10@gmail.com>
2026-04-05 22:32:08 +0500

    add random logic (position & color)

---
+++ src/components/gameBoard.jsx
@@ -0,0 +1,10 @@
+import "../assets/css/gameBoard.css";
+function GameBoard() {
+    return (
+        <div className="game-board">
+
+        </div>
+    )
+}
+
+export default GameBoard;
---
+++ src/utils/random.js
@@ -0,0 +1,16 @@
+//INFO Generate the 50 colors once
+const generatePalette = (amount) => {
+  return Array.from({ length: amount }, (_, i) => {
+    return `hsl(${i * (360 / amount)}, 70%, 60%)`;
+  });
+};
+
+const fiftyColors = generatePalette(50);
+
+//INFO Export a function to pick one
+export const getRandomPaletteColor = () => {
+  const randomIndex = Math.floor(Math.random() * fiftyColors.length);
+  return fiftyColors[randomIndex];
+};
+
+export const getRandomPos = () => Math.floor(Math.random() * 90);
